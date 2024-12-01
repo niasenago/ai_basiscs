038cb51ab377df5b1b3e5b4a96fb273adfcc2a66
Artiom Hovhannisyan <a_hovhannisyan@wargaming.net>
2024-12-01 12:37:10 +0200

    add function to create a hardcoded model

--- lab_4/main.py
+++ lab_4/main.py
@@ -47,6 +47,36 @@ def build_model_from_json(json_path, input_shape=None):
 
     return model
 
+def build_model_hardcoded(json_path, input_shape):
+    with open(json_path, 'r') as file:
+        config = json.load(file)
+    
+    training_config = config.get("training", {})
+    optimizer = training_config.get("optimizer", "adam") #default is adam
+    loss_function = training_config.get("loss_function", "binary_crossentropy") # default is binary_crossentropy
+    metrics = ["accuracy"]  
+
+    # hardcoded architecture
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.25))
+    ###
+    # The Dropout layer randomly sets input units to 0 with a frequency of rate at each step during training time,
+    # which helps prevent overfitting. 
+    # Inputs not set to 0 are scaled up by 1 / (1 - rate) such that the sum over all inputs is unchanged.
+    #
+
+    model.add(Flatten())
+    model.add(Dense(units=1, activation='sigmoid'))  
+
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=metrics)
+
+    return model
+
+
 def load_data(path):
     pass
     # Load dataset
@@ -74,7 +104,7 @@ def main():
     epochs, brach_size, kernel_size = 20, 32, 9
     
     input_shape = (28, 28, 1)  # Example input shape for grayscale image data
-    model = build_model_from_json("hyperparams.json", input_shape=input_shape)
+    model = build_model_hardcoded("hyperparams.json", input_shape=input_shape)
 
     model.summary()
    # model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
