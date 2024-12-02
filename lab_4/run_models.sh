7b630927b04f38a212d9f489e3d86821eac7891c
Artiom Hovhannisyan <a_hovhannisyan@wargaming.net>
2024-12-02 10:14:33 +0200

    mv hyperparams to dedicated folder; add running script

---
+++ lab_4/run_models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 main.py --hyperparams hyperparams_2.json --model_name "2_model"
+
+python3 main.py --hyperparams hyperparams_3.json --model_name "3_model"
+
+python3 main.py --hyperparams hyperparams_4.json --model_name "4_model"
+
+python3 main.py --hyperparams hyperparams_5.json --model_name "5_model"
+
+python3 main.py --hyperparams hyperparams_6.json --model_name "6_model"
